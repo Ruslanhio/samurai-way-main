6c12a6f0c872ae315ca239ea239c0965cfee5ea4
Ruslanhio <ruslanhio2891@gmail.com>
2022-10-21 21:12:46 +0300

    error

---
+++ src/renderTree.tsx
@@ -0,0 +1,11 @@
+import {store} from './componets/redux/state';
+import ReactDOM from 'react-dom';
+import App from './App';
+import React from 'react';
+
+export const renderTree = () => {
+    ReactDOM.render(
+        <App store={store}  />,
+        document.getElementById('root')
+    );
+}
